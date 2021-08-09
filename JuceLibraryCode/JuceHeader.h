618e5a2a9821268e4b2f750a1ddfdbc84149a105
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2021-08-09 20:11:58 +0200

    Ver.: --> 8.6.4.37

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -49,7 +49,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "GuitarFineTune";
     const char* const  companyName    = "eksit";
-    const char* const  versionString  = "8.5.3";
-    const int          versionNumber  = 0x80503;
+    const char* const  versionString  = "8.6.4";
+    const int          versionNumber  = 0x80604;
 }
 #endif
