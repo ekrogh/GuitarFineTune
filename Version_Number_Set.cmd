6c027724f51fc0a5a7104943a0c3b0fe37b795a0
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2022-06-04 20:01:10 +0200

    start /w call code-insiders.cmd .\snap\snapcraft.yaml .\snap\gui\guitarfinetune.desktop

--- Version_Number_Set.cmd
+++ Version_Number_Set.cmd
@@ -1,4 +1,4 @@
 start /w ..\projucer\Projucer.exe .\GuitarFineTune.jucer
-start /w "D:\Program_Files\Microsoft_VS_Code_Insiders\Code - Insiders.exe" .\snap\snapcraft.yaml .\snap\gui\guitarfinetune.desktop
+start /w call code-insiders.cmd .\snap\snapcraft.yaml .\snap\gui\guitarfinetune.desktop
 start /w D:\Program_Files\Microsoft_Visual_Studio\2022\Community\Common7\IDE\devenv.exe .\Builds\VisualStudio2022\GuitarFineTune.sln
 D:\Program_Files\SmartGit\bin\smartgit.exe .
