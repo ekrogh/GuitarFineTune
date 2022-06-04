b3464c87f6647eccb1b7012fe0cd6ca89b90b874
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2022-06-04 19:47:22 +0200

    "D:\Program_Files\Microsoft_VS_Code_Insiders\Code - Insiders.exe"

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -49,7 +49,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "GuitarFineTune";
     const char* const  companyName    = "eksit";
-    const char* const  versionString  = "9.3.4";
-    const int          versionNumber  = 0x90304;
+    const char* const  versionString  = "9.3.5";
+    const int          versionNumber  = 0x90305;
 }
 #endif
