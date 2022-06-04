06cf70b4932e4465036edc0e18c33c518bf7b8fb
eigil <eigilkrogh@icloud.com>
2022-06-04 18:37:43 +0200

    Clean.

--- Version_Number_Set.cmd
+++
@@ -1,4 +0,0 @@
-..\projucer\Projucer.exe .\GuitarFineTune.jucer
-call code-insiders.cmd .\snap\snapcraft.yaml .\snap\gui\guitarfinetune.desktop
-D:\Program_Files\Microsoft_Visual_Studio\2022\Community\Common7\IDE\devenv.exe .\Builds\VisualStudio2022\GuitarFineTune.sln
-D:\Program_Files\SmartGit\bin\smartgit.exe .
