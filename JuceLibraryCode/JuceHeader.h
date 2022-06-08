22b030706c4a651b21bb0c3430cb14fe011ba36c
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2022-06-08 18:36:00 +0200

    New packProj added.

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -49,7 +49,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "GuitarFineTune";
     const char* const  companyName    = "eksit";
-    const char* const  versionString  = "9.4.6";
-    const int          versionNumber  = 0x90406;
+    const char* const  versionString  = "9.4.7";
+    const int          versionNumber  = 0x90407;
 }
 #endif
