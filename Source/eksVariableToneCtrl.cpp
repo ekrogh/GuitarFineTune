3d424517c6abc6b3fef1ea6d5f3176294e8cb990
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2022-10-11 11:24:08 +0200

    eksVariableToneCtrl again

--- Source/eksVariableToneCtrl.cpp
+++ Source/eksVariableToneCtrl.cpp
@@ -31,69 +31,69 @@ extern std::shared_ptr<AudioDeviceManager> sharedAudioDeviceManager;
 //[/MiscUserDefs]
 
 //==============================================================================
-eksVariableToneCtrl::eksVariableToneCtrl (        std::shared_ptr<xmlGuitarFineTuneConfig> pXmlGFTConfig          , std::shared_ptr<eksLookAndFeel> pGFTAF  )
-    : DocumentWindow("Variable Tone Control", Colour(0xFF2B0720), DocumentWindow::allButtons)
-      //pGuitarFineTuneLookAndFeel(pGFTAF),
-      //pXmlGuitarFineTuneConfig(pXmlGFTConfig),
+eksVariableToneCtrl::eksVariableToneCtrl(std::shared_ptr<xmlGuitarFineTuneConfig> pXmlGFTConfig, std::shared_ptr<eksLookAndFeel> pGFTAF)
+	: DocumentWindow("Variable Tone Control", Colour(0xFF2B0720), DocumentWindow::allButtons),
+	pGuitarFineTuneLookAndFeel(pGFTAF),
+	pXmlGuitarFineTuneConfig(pXmlGFTConfig)
 {
-    //[Constructor_pre] You can add your own custom stuff here..
-    //[/Constructor_pre]
+	//[Constructor_pre] You can add your own custom stuff here..
+	//[/Constructor_pre]
 
-    FrequenceSlider.reset (new juce::Slider ("FrequenceSlider"));
-    addAndMakeVisible (FrequenceSlider.get());
-    FrequenceSlider->setRange (0, 20000, 0);
-    FrequenceSlider->setSliderStyle (juce::Slider::LinearVertical);
-    FrequenceSlider->setTextBoxStyle (juce::Slider::TextBoxLeft, false, 80, 20);
-    FrequenceSlider->addListener (this);
+	FrequenceSlider.reset(new juce::Slider("FrequenceSlider"));
+	addAndMakeVisible(FrequenceSlider.get());
+	FrequenceSlider->setRange(0, 20000, 0);
+	FrequenceSlider->setSliderStyle(juce::Slider::LinearVertical);
+	FrequenceSlider->setTextBoxStyle(juce::Slider::TextBoxLeft, false, 80, 20);
+	FrequenceSlider->addListener(this);
 
-    FrequenceSlider->setBounds (0, 0, 150, 448);
-
-
-    //[UserPreSize]
-    setContentOwned(FrequenceSlider.get(), true);
-    //[/UserPreSize]
-
-    setSize (600, 400);
+	FrequenceSlider->setBounds(0, 0, 150, 448);
 
 
-    //[Constructor] You can add your own custom stuff here..
-    //[/Constructor]
+	//[UserPreSize]
+	setContentOwned(FrequenceSlider.get(), true);
+	//[/UserPreSize]
+
+	setSize(600, 400);
+
+
+	//[Constructor] You can add your own custom stuff here..
+	//[/Constructor]
 }
 
 //==============================================================================
-void eksVariableToneCtrl::paint (juce::Graphics& g)
+void eksVariableToneCtrl::paint(juce::Graphics& g)
 {
-    //[UserPrePaint] Add your own custom painting code here..
-    //[/UserPrePaint]
+	//[UserPrePaint] Add your own custom painting code here..
+	//[/UserPrePaint]
 
-    g.fillAll (juce::Colour (0xff2b0720));
+	g.fillAll(juce::Colour(0xff2b0720));
 
-    //[UserPaint] Add your own custom painting code here..
-    //[/UserPaint]
+	//[UserPaint] Add your own custom painting code here..
+	//[/UserPaint]
 }
 
 void eksVariableToneCtrl::resized()
 {
-    //[UserPreResize] Add your own custom resize code here..
-    //[/UserPreResize]
+	//[UserPreResize] Add your own custom resize code here..
+	//[/UserPreResize]
 
-    //[UserResized] Add your own custom resize handling here..
-    //[/UserResized]
+	//[UserResized] Add your own custom resize handling here..
+	//[/UserResized]
 }
 
-void eksVariableToneCtrl::sliderValueChanged (juce::Slider* sliderThatWasMoved)
+void eksVariableToneCtrl::sliderValueChanged(juce::Slider* sliderThatWasMoved)
 {
-    //[UsersliderValueChanged_Pre]
-    //[/UsersliderValueChanged_Pre]
+	//[UsersliderValueChanged_Pre]
+	//[/UsersliderValueChanged_Pre]
 
-    if (sliderThatWasMoved == FrequenceSlider.get())
-    {
-        //[UserSliderCode_FrequenceSlider] -- add your slider handling code here..
-        //[/UserSliderCode_FrequenceSlider]
-    }
+	if (sliderThatWasMoved == FrequenceSlider.get())
+	{
+		//[UserSliderCode_FrequenceSlider] -- add your slider handling code here..
+		//[/UserSliderCode_FrequenceSlider]
+	}
 
-    //[UsersliderValueChanged_Post]
-    //[/UsersliderValueChanged_Post]
+	//[UsersliderValueChanged_Post]
+	//[/UsersliderValueChanged_Post]
 }
 
 
@@ -106,22 +106,22 @@ void eksVariableToneCtrl::sliderValueChanged (juce::Slider* sliderThatWasMoved)
 #if 0
 /*  -- Projucer information section --
 
-    This is where the Projucer stores the metadata that describe this GUI layout, so
-    make changes in here at your peril!
+	This is where the Projucer stores the metadata that describe this GUI layout, so
+	make changes in here at your peril!
 
 BEGIN_JUCER_METADATA
 
 <JUCER_COMPONENT documentType="Component" className="eksVariableToneCtrl" componentName=""
-                 parentClasses="public juce::DocumentWindow" constructorParams="        std::shared_ptr&lt;xmlGuitarFineTuneConfig&gt; pXmlGFTConfig          , std::shared_ptr&lt;eksLookAndFeel&gt; pGFTAF  "
-                 variableInitialisers="pGuitarFineTuneLookAndFeel(pGFTAF)&#10;pXmlGuitarFineTuneConfig(pXmlGFTConfig)&#10;DocumentWindow(&quot;Variable Tone Control&quot;, Colour(0xFF2B0720), DocumentWindow::allButtons)"
-                 snapPixels="8" snapActive="1" snapShown="1" overlayOpacity="0.330"
-                 fixedSize="0" initialWidth="600" initialHeight="400">
+				 parentClasses="public juce::DocumentWindow" constructorParams="        std::shared_ptr&lt;xmlGuitarFineTuneConfig&gt; pXmlGFTConfig          , std::shared_ptr&lt;eksLookAndFeel&gt; pGFTAF  "
+				 variableInitialisers="pGuitarFineTuneLookAndFeel(pGFTAF)&#10;pXmlGuitarFineTuneConfig(pXmlGFTConfig)&#10;DocumentWindow(&quot;Variable Tone Control&quot;, Colour(0xFF2B0720), DocumentWindow::allButtons)"
+				 snapPixels="8" snapActive="1" snapShown="1" overlayOpacity="0.330"
+				 fixedSize="0" initialWidth="600" initialHeight="400">
   <BACKGROUND backgroundColour="ff2b0720"/>
   <SLIDER name="FrequenceSlider" id="b44be89bb3688130" memberName="FrequenceSlider"
-          virtualName="" explicitFocusOrder="0" pos="0 0 150 448" min="0.0"
-          max="20000.0" int="0.0" style="LinearVertical" textBoxPos="TextBoxLeft"
-          textBoxEditable="1" textBoxWidth="80" textBoxHeight="20" skewFactor="1.0"
-          needsCallback="1"/>
+		  virtualName="" explicitFocusOrder="0" pos="0 0 150 448" min="0.0"
+		  max="20000.0" int="0.0" style="LinearVertical" textBoxPos="TextBoxLeft"
+		  textBoxEditable="1" textBoxWidth="80" textBoxHeight="20" skewFactor="1.0"
+		  needsCallback="1"/>
 </JUCER_COMPONENT>
 
 END_JUCER_METADATA
