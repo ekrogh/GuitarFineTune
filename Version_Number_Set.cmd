0e3524e5f3d8a7dc6a7ddbc433b0802c33dbdf84
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2022-10-13 09:39:56 +0200

    Ver.: 9.9.5(.67)

--- Version_Number_Set.cmd
+++ Version_Number_Set.cmd
@@ -1,4 +1,4 @@
 start /w ..\projucer\Projucer.exe .\GuitarFineTune.jucer
 call code.cmd .\snap\snapcraft.yaml .\snap\gui\guitarfinetune.desktop
 start /w D:\Program_Files\Microsoft_Visual_Studio\2022\Community\Common7\IDE\devenv.exe .\Builds\VisualStudio2022\GuitarFineTune.sln
-D:\Program_Files\SmartGit\bin\smartgit.exe .
+D:\Program_Files\SmartGit_22.1\bin\smartgit.exe .
